19a5d7991f045dd21279b3f3fcd001f54a277907
Nicola Giannelli <ghesio@gmail.com>
2021-01-28 16:59:58 +0100

    refactor of data augmentation

--- utils/data_augmentation.py
+++ utils/data_augmentation.py
@@ -7,25 +7,23 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-def random_rotations(img, angles=(-10, 10)):
+def rotate_image(img, angle):
     """
     Return a rotated image using a random angle
     :param img: the image to be rotated
-    :param angles: the angle range
+    :param angle: the angle
     :return: the rotated image
     """
-    rotation = np.random.randint(angles[0], angles[1])
-    return rotate(img, rotation, reshape=False)
+    return rotate(img, angle, reshape=False)
 
 
-def cv2_clipped_zoom(img, zoom=(0.85, 1.20)):
+def zoom_image(img, zoom_factor):
     """
     Return a zoomed image using a random zoom
     :param img: the image to be zoomed
-    :param zoom: the zoom range
+    :param zoom_factor: the zoom factor
     :return: the zoomed image
     """
-    zoom_factor = np.random.randint(zoom[0], zoom[1])
     height, width = img.shape[:2] # It's also the final desired shape
     new_height, new_width = int(height * zoom_factor), int(width * zoom_factor)
 
@@ -50,26 +48,25 @@ def cv2_clipped_zoom(img, zoom=(0.85, 1.20)):
     return result
 
 
-def shift_image(img, shift=(-10, 10), axis=1):
+def shift_image(img, shift, axis=1):
     """
     Shift an image horizontally or vertically using a random value
     :param img: the image to be shifted
-    :param shift: the shift range
+    :param shift: the shift factor
     :param axis: the axis for the switch, 1 for horizontal, 0 for vertical
     :return: the shifted image
     """
-    delta = np.random.randint(shift[0], shift[1])
-    img = np.roll(img, delta, axis)
-    if delta > 0:
+    img = np.roll(img, shift, axis)
+    if shift > 0:
         if axis is 1:
-            img[:, :delta] = 0
+            img[:, :shift] = 0
         else:
-            img[:delta, :] = 0
-    elif delta < 0:
+            img[:shift, :] = 0
+    elif shift < 0:
         if axis is 1:
-            img[:, delta:] = 0
+            img[:, shift:] = 0
         else:
-            img[delta:, :] = 0
+            img[shift:, :] = 0
     return img
 
 
@@ -79,15 +76,14 @@ def test():
     :return: void
     """
     img = face()
-    rot = random_rotations(img, [10, 30])
-    zoom = cv2_clipped_zoom(img, [10, 20])
-    horizontal = shift_image(img, [-300, 300], axis=1)
-    vertical = shift_image(img, [-300, 300], axis=0)
-
+    rot = rotate_image(img, 10)
+    zoomed = zoom_image(img, 50)
+    horizontal = shift_image(img, -300, axis=1)
+    vertical = shift_image(img, 300, axis=0)
     fig, ax = plt.subplots(2, 3)
     ax[0][0].imshow(img)
     ax[0][1].imshow(rot)
-    ax[1][0].imshow(zoom)
+    ax[1][0].imshow(zoomed)
     ax[1][1].imshow(horizontal)
     ax[1][2].imshow(vertical)
 
