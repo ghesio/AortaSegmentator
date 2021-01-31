a95690b0e54fe5a5f66f02c7c1c52c91d45b3dea
Nicola Giannelli <ghesio@gmail.com>
2021-01-31 18:53:43 +0100

    Added early stopping and time logging

--- training/2d_network_training.py
+++ training/2d_network_training.py
@@ -8,6 +8,8 @@ from utils import data_loader as dl
 from keras.models import Model
 from keras.callbacks import ModelCheckpoint
 import tensorflow as tf
+from datetime import datetime
+
 
 # define backbone for the net
 backbone = 'resnet34'
@@ -15,7 +17,7 @@ backbone = 'resnet34'
 direction = "axial"
 # network parameter
 batch_size = 16
-epochs = 2
+epochs = 1
 # load data
 (x_train, x_test, x_val, y_train, y_test, y_val) = dl.get_data_set(direction=direction, augmentation=False)
 # add channel info to train set
@@ -31,24 +33,29 @@ model = Model(inp, out, name=base_model.name)
 # compile the model
 model.compile('Adam', loss=sm.losses.bce_jaccard_loss, metrics=[sm.metrics.iou_score])
 # define callbacks for saving checkpoints
-callbacks = [
-    # this callback saves a SavedModel every X batches
-    ModelCheckpoint(
-        filepath='../checkpoints/'+direction+'_'+backbone+'-loss={loss:.2f}',
-        save_freq=1)
-]
+save_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath='../checkpoints/'+direction+'_'+backbone+'-{epoch:02d}-{val_loss:.2f}.hdf5',
+    save_weights_only=True,
+    monitor='val_loss',
+    mode='min',
+    save_best_only=True)
+# early stopping criteria
+early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', patience=5)
 # fit the model
+print("Training start @ ", datetime.now().strftime("%H:%M:%S"))
 history = model.fit(
    x=x_train,
    y=y_train,
    batch_size=batch_size,
    epochs=epochs,
    validation_data=(x_val, y_val),
-   callbacks=callbacks
+   callbacks=[save_callback, early_stopping_callback]
 )
-
-print(history.history)
+print("Training end @ ", datetime.now().strftime("%H:%M:%S"))
+print("\r\nTraining results")
+for key in history.history.keys():
+    print(key+':', history.history[key])
+print("\r\nTest evaluation")
 results = model.evaluate(x_test, y_test, batch_size=16)
-print(results)
-print("test loss, test acc:", results)
-
+print('Loss:', results[0], 'Accuracy:', results[1])
+exit(0)
