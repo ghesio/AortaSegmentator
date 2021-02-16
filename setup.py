5702bb76a799510ba56992dd5d41dea1764b88ad
Nicola Giannelli <ghesio@gmail.com>
2021-02-16 11:57:22 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AortaSegmentator',
+    version='0.1a',
+    packages=['utils', 'training', 'test_evaluation', 'data_preprocessing'],
+    url='https://github.com/ghesio/AortaSegmentator',
+    license='MIT',
+    author='ghesio',
+    author_email='ghesio@gmail.com',
+    description='AortaSegmentator'
+)
