6891ea28a1d2c827a9711d7ade8549b78a3fa334
Ghesio <ghesio@gmail.com>
2021-03-22 16:02:59 +0100

    WIP - test evaluation

--- test_evaluation/test_evaluation.py
+++ test_evaluation/test_evaluation.py
@@ -19,10 +19,6 @@ with open('data/info.json') as f:
         if patient_map[patient]['partition'] == 'test':
             directories.append((patient_map[patient]['scan_dir'], patient_map[patient]['roi_dir']))
 equalization = True
-# load file names for generating the dump file name
-best_files = get_best_checkpoints()
-timestamp = datetime.today().strftime('%Y%m%d_%H%M%S')
-dump_file_name = 'checkpoints//' + best_files[0][17:-10].replace('axial_', '') + '-' + timestamp + '.npz'
 # load models [0] = axial, [1]=coronal, [2]=sagittal
 models = get_pretrained_models()
 # vectors to store the predictions on file system
@@ -69,8 +65,62 @@ for i in range(len(directories)):
     array_coronal.append(prediction_coronal)
     array_sagittal.append(prediction_sagittal)
     array_combined.append(prediction_combined)
-logging.info('Saving results')
-np.savez_compressed(file=dump_file_name, axial=array_axial, coronal=array_coronal,
-                    sagittal=array_sagittal, combined=array_combined, roi=array_combined)
+
+thresholds = np.arange(threshold_interval[0], threshold_interval[1], delta)
+# iterate and calculate IoU score
+for threshold in thresholds:
+    threshold = round(threshold, 2)
+    iou_map_axial[threshold] = []
+    iou_map_coronal[threshold] = []
+    iou_map_sagittal[threshold] = []
+    iou_map_combined[threshold] = []
+    logging.info("Evaluating threshold " + str(threshold))
+    for i in range(array_axial.shape[0]):
+        prediction_axial = np.copy(array_axial[i])
+        prediction_coronal = np.copy(array_coronal[i])
+        prediction_sagittal = np.copy(array_sagittal[i])
+        prediction_combined = np.copy(array_combined[i])
+        roi_array = roi[i]
+        prediction_axial[prediction_axial >= threshold] = 1
+        prediction_coronal[prediction_coronal >= threshold] = 1
+        prediction_sagittal[prediction_sagittal >= threshold] = 1
+        prediction_combined[prediction_combined >= threshold] = 1
+        axial_iou_score = calculate_iou_score(prediction=prediction_axial, ground_truth=roi_array)
+        iou_map_axial[threshold].append(axial_iou_score)
+        coronal_iou_score = calculate_iou_score(prediction=prediction_coronal, ground_truth=roi_array)
+        iou_map_coronal[threshold].append(coronal_iou_score)
+        sagittal_iou_score = calculate_iou_score(prediction=prediction_sagittal, ground_truth=roi_array)
+        iou_map_sagittal[threshold].append(sagittal_iou_score)
+        combined_iou_score = calculate_iou_score(prediction=prediction_combined, ground_truth=roi_array)
+        iou_map_combined[threshold].append(combined_iou_score)
+        logging.debug('IoU scores (axial, coronal, sagittal, combined): ' + str(axial_iou_score) + ' '
+                       + str(coronal_iou_score) + ' ' + str(sagittal_iou_score) + ' ' + str(combined_iou_score))
+
+filename = '../data/results_' + timestamp + '.tsv'
+logging.info('Saving tsv file - ' + filename)
+with open(filename, 'w', newline='') as file:
+    writer = csv.writer(file, delimiter='\t')
+    writer.writerow(['threshold', 'type', 'iou_scores', 'iou_mean', 'iou_std', 'iou_var'])
+    # axial
+    for threshold in iou_map_axial:
+        writer.writerow([threshold, 'axial', str(iou_map_axial[threshold])[1:-1],
+                         round(float(np.mean(iou_map_axial[threshold])), 4),
+                         round(float(np.std(iou_map_axial[threshold])), 4),
+                         round(float(np.var(iou_map_axial[threshold])), 4)])
+    for threshold in iou_map_coronal:
+        writer.writerow([threshold, 'coronal', str(iou_map_coronal[threshold])[1:-1],
+                         round(float(np.mean(iou_map_coronal[threshold])), 4),
+                         round(float(np.std(iou_map_coronal[threshold])), 4),
+                         round(float(np.var(iou_map_coronal[threshold])), 4)])
+    for threshold in iou_map_sagittal:
+        writer.writerow([threshold, 'sagittal', str(iou_map_sagittal[threshold])[1:-1],
+                         round(float(np.mean(iou_map_sagittal[threshold])), 4),
+                         round(float(np.std(iou_map_sagittal[threshold])), 4),
+                         round(float(np.var(iou_map_sagittal[threshold])), 4)])
+    for threshold in iou_map_combined:
+        writer.writerow([threshold, 'combined', str(iou_map_combined[threshold])[1:-1],
+                         round(float(np.mean(iou_map_combined[threshold])), 4),
+                         round(float(np.std(iou_map_combined[threshold])), 4),
+                         round(float(np.var(iou_map_combined[threshold])), 4)])
 exit(0)
 
